95781662322bc9bd0ac1a3192ac38a0bdfab9b83
AdeelAtta <adeelatta@gmail.com>
2023-04-20 20:45:48 +0500

    gallery add picture form added

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -1,6 +1,8 @@
 import React, { useState } from 'react'
+import $ from 'jquery'
 import Card from './Card'
 import img from '../images/register1.png'
+import '../cssfiles/gallery.css'
 import {
     MDBBtn,
     MDBModal,
@@ -12,6 +14,52 @@ import {
 } from 'mdb-react-ui-kit';
 const Gallery = () => {
     const [optSmModal, setOptSmModal] = useState(false);
+
+
+    // 
+    const [selectedFile, setSelectedFile] = useState(null);
+
+    function handleImageUpload(e) {
+        e.preventDefault();
+
+        // Handle image upload logic here
+    }
+
+    function handleFileInputChange(e) {
+        setSelectedFile(e.target.files[0]);
+        readURL(e.target);
+    }
+
+    function removeImage() {
+        setSelectedFile(null);
+        $('.file-upload-input').replaceWith($('.file-upload-input').clone());
+        $('.file-upload-content').hide();
+        $('.image-upload-wrap').show();
+    }
+
+    function readURL(input) {
+        if (input.files && input.files[0]) {
+            const reader = new FileReader();
+
+            reader.onload = function (e) {
+                $('.image-upload-wrap').hide();
+                $('.file-upload-image').attr('src', e.target.result);
+                $('.file-upload-content').show();
+                $('.image-title').html(input.files[0].name);
+            };
+
+            reader.readAsDataURL(input.files[0]);
+        } else {
+            removeImage();
+        }
+    }
+
+
+
+
+
+
+
     return (
         <div className='row gap-2 py-4'>
             <div className="col-12 m-auto ps-0 d-flex justify-content-between">
@@ -39,6 +87,55 @@ const Gallery = () => {
                             <MDBBtn className='btn-close' color='none' onClick={() => setOptSmModal(!optSmModal)}></MDBBtn>
                         </MDBModalHeader>
                         <MDBModalBody>
+
+                            <form onSubmit={handleImageUpload}>
+                                <div className="formbold-mb-3">
+                                    <label htmlFor="title" className="formbold-form-label">
+                                        Title
+                                    </label>
+                                    <input
+                                        type="text"
+                                        name="title"
+                                        id="title"
+                                        placeholder="Title here."
+                                        className="formbold-form-input"
+                                    />
+                                </div>
+
+                                <div className="image-upload-wrap">
+                                    <input
+                                        className="file-upload-input"
+                                        type="file"
+                                        onChange={handleFileInputChange}
+                                        accept="image/*"
+                                    />
+                                    <div className="drag-text">
+                                        <h3>Drag and drop a file or select add Image</h3>
+                                    </div>
+                                </div>
+
+                                {selectedFile && (
+                                    <div className="file-upload-content">
+                                        <img
+                                            className="file-upload-image"
+                                            src="#"
+                                            alt="your image"
+                                        />
+                                        <div className="image-title-wrap">
+                                            <button type="button" onClick={removeImage} className="remove-image">
+                                                Remove <span className="image-title">Uploaded Image</span>
+                                            </button>
+                                        </div>
+                                    </div>
+                                )}
+
+                                <div className=" text-center mb-2">
+                                    <button className="formbold-btn btn_lg" type="submit">
+                                        Upload
+                                    </button>
+                                </div>
+                            </form>
+
 
 
 
