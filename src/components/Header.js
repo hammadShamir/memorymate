5e145463db0525a553c54bdf5ef5694a2402e41c
hammadShamir <hammadShamir7@gmail.com>
2023-04-21 18:51:19 +0500

    Change logo and add Menu Dropdown

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,19 @@
 import React, { useState, useEffect } from 'react';
 import { Link, useLocation } from 'react-router-dom'
 import logo from '../../src/images/logo.png'
+import ProfileDropdown from './ProfileMenu';
 const Header = ({ user }) => {
     const location = useLocation();
     const currentUrl = location.pathname;
 
 
     const token = localStorage.getItem("accessToken")
+    const styles = {
+        backgroundColor: '#071D3B',
+        color: '#fff',
+        transition: 'background-color 0.3s',
 
+    };
     return (
         <nav id='menubar' className="navbar navbar-expand-lg navbar-light bg-light">
             <div className="container">
@@ -17,18 +23,14 @@ const Header = ({ user }) => {
                 <form className="d-flex">
                     {
                         token ? (
-                            <Link to="/signin" className="btn btn-outline-warning" type="submit" onClick={() => {
-                                localStorage.removeItem('accessToken');
-                            }}>
-                                Sign Out
-                            </Link>
+                            <ProfileDropdown />
                         ) : (
                             currentUrl === "/signin" ? (
-                                <Link to="/signup" className="btn btn-outline-warning" type="submit">
+                                <Link to="/signup" className="btn header_btn" type="submit" style={styles}>
                                     Sign UP
                                 </Link>
                             ) : (
-                                <Link to="/" className="btn btn-outline-warning" type="submit">
+                                <Link to="/" className="btn header_btn" type="submit" style={styles}>
                                     Sign In
                                 </Link>
                             )
---
+++ src/components/ProfileMenu.js
@@ -0,0 +1,47 @@
+import React, { useState, useEffect } from 'react';
+import { auth } from '../Firebase';
+import { useNavigate } from 'react-router-dom';
+
+
+const ProfileDropdown = () => {
+    const [user, setUser] = useState(null)
+    const [isOpen, setIsOpen] = useState(false);
+    const navigate = useNavigate();
+    const handleToggle = () => {
+        setIsOpen(!isOpen);
+    };
+    const handleSignOutClick = () => {
+        localStorage.removeItem('accessToken');
+        navigate('/signin')
+    };
+
+    useEffect(() => {
+        const unsubscribe = auth.onAuthStateChanged(user => {
+            if (user) {
+                setUser(user);
+            } else {
+                setUser(null);
+            }
+        });
+        return () => unsubscribe();
+    }, []);
+
+
+    return (
+        <div className="dropdown">
+            <button
+                style={{ backgroundColor: `#071D3B`, color: `#fff` }}
+                className="btn dropdown-toggle"
+                type="button"
+                id="profile-dropdown"
+                onClick={handleToggle}
+            >
+                {user ? user.displayName : 'Guest'}
+            </button>
+            <div className={`dropdown-menu ${isOpen ? 'show' : ''}`} aria-labelledby="profile-dropdown" >
+                <button className="dropdown-item" onClick={handleSignOutClick} >Sign Out</button>
+            </div>
+        </div>
+    );
+}
+export default ProfileDropdown;
